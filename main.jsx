85fb8b6ecc233131fe2d3d24aa3a5c359fa535ae
shiba-2007 <shiba18012007@gmail.com>
2025-12-24 13:53:51 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css"
-import App from './new1.jsx';
+import App from './task2.jsx';
 
 
 createRoot(document.getElementById('root')).render(
